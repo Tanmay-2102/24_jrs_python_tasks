e6855c15f1a9422a8534ac3a824a8be17ca67a18
Tanmay <160754457+Tanmay-2102@users.noreply.github.com>
2024-06-26 00:18:03 +0530

    Solution.py

--- lec_1/solution.py
+++ lec_1/solution.py
@@ -18,3 +18,15 @@
     #print(fact)
 #else:
     #print("Not a valid input")
+
+#Q3
+#Computers ={
+  #"Laptop1" : {"brand" : "DELL","OS" : "Windows"},
+  #"Laptop2" : {"brand" : "HP", "OS" : "Linux"},
+  #"Desktop" : {"brand" : "Apple","OS" : "Mac-OS"}
+#}
+#brand=[Computers["Laptop1"]["brand"],Computers["Laptop2"]["brand"],Computers["Desktop"]["brand"]]
+#OS=[Computers["Laptop1"]["OS"],Computers["Laptop2"]["OS"],Computers["Desktop"]["OS"]]
+#print(brand)
+#print(OS)
+
