a68027de8c797a3f3c42c33dded8349b45f71d62
Tanmay <160754457+Tanmay-2102@users.noreply.github.com>
2024-06-26 00:10:13 +0530

    Solution.py

--- lec_1/solution.py
+++ lec_1/solution.py
@@ -1,1 +1,20 @@
-#write your answer here
+#Q1
+
+#count = 0
+#n = int(input("Enter a number:"))
+#while n != 0:
+    #n = int(n/10)
+    #count += 1
+#print("Number of Digits:", count)
+
+#Q2
+
+#fact = 1
+#n = input("Enter a number:")
+#if n.isdigit()==True:
+    #n=int(n)
+    #for i in range(1, n + 1):
+        #fact = i*fact
+    #print(fact)
+#else:
+    #print("Not a valid input")
